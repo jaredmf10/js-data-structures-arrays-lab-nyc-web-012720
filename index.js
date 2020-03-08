1a022b76b617e8cc66e2c5c21e823d0d706ccd07
Jared Fleming <jaredmf10@gmail.com>
2020-03-08 22:49:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ let drivers = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver (name) {
   drivers.push(name);
 }
+
+function destructivelyPrependDriver (name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver (name) {
+  drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver (name) {
+  drivers.shift(name);
+}
+
+function appendDriver (name) {
+  return [...drivers, name];
+}
